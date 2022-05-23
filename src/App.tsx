4cc642f94bc60b84a752348b05b89de741d3fa2a
Hanan88 <Hanan.Ahmed.8@hotmail.com>
2022-05-23 12:20:58 +0200

    typescript: install

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Greeting from './components/Greeting.tsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Greeting name="Mohamed" />
+    </div>
+  );
+}
+
+export default App;
