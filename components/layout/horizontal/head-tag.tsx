dfa04a687df80ba0fa439c1ea5b03d73b7c99580
Mohamad Eldhemy <imdhemy@gmail.com>
2022-06-18 15:26:29 +0300

    feat(layout): Implement a horizontal head-tag component

---
+++ components/layout/horizontal/head-tag.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import config from "../../../config";
+
+type HeadTagProps = {
+  title?: string;
+};
+
+const HeadTag = (props: HeadTagProps) => {
+  const suffix = props.title ? `| ${config.app.name}` : '';
+  let title = props.title || `${config.app.name}: ${config.app.slogan}`;
+  if (suffix != '') title = `${title} ${suffix}`;
+
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8"/>
+        <title>{title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta content={config.app.description} name="description"/>
+      </Head>
+    </>
+  );
+};
+
+export default HeadTag;
---
+++ config/index.ts
@@ -0,0 +1,9 @@
+const config = {
+  app: {
+    name: 'Dolphin',
+    slogan: 'Customer Feedback Management Tool',
+    description: 'Dolphin helps you collect and organize feature requests to better understand customer needs and prioritize your roadmap.',
+  },
+};
+
+export default config;
