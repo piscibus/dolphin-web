5480961be084469fd61083cce68ea6bbf181db6b
Mohamad Eldhemy <imdhemy@gmail.com>
2022-06-26 10:44:47 +0300

    chore(icons): install feather icons

--- components/layout/horizontal/scripts.tsx
+++ components/layout/horizontal/scripts.tsx
@@ -6,7 +6,6 @@ const Scripts = () => (
     <Script src='/assets/libs/bootstrap/js/bootstrap.bundle.min.js'/>
     <Script src='/assets/libs/simplebar/simplebar.min.js'/>
     <Script src='/assets/libs/node-waves/waves.min.js'/>
-    <Script src='/assets/libs/feather-icons/feather.min.js'/>
     <Script src='/assets/libs/pace-js/pace.min.js'/>
   </>
 );
